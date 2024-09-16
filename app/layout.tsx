2ba54292cc5da4d1d0b707a92300e8eb2be92517
egomez3412 <egomez3412@gmail.com>
2024-09-15 23:05:49 -0700

    fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import { NavigationComponent } from '@/components/src-app-navigation'
 import { FooterComponent } from '@/components/src-components-footer'
 import ConditionalContent from '../components/conditional-component'
-import Head from 'next/head'
 import './globals.css'
 
 export const metadata = {
   title: 'Guerrero Foto Y Video',
   description: 'Professional photography and video services',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
@@ -16,9 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="./favicon.ico" />
-      </Head>
       <body className="flex flex-col min-h-screen">
         <NavigationComponent />
         <main className="flex-grow mt-16 p-4 pt-20">
